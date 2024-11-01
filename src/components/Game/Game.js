c1e2c423982bc59c92b4bc8b3d378e6967f64a6e
Greg Plumbly <greg.plumbly@gmail.com>
2024-11-01 21:09:27 +0000

    display guesses.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
+import Guess from '../Guess';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -23,6 +24,7 @@ function Game() {
   return (
     <div>
       <GuessResults guessList={guessList} />
+      <Guess guessList={guessList} />
       <GuessInput onSubmitGuess={handleSubmitGuess} />
     </div>
   );
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import Guess from '../Guess';
 
 function GuessResults({ guessList }) {
   return (
     <div className="guess-results">
-      {guessList.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map(num => (
+        <Guess key={num} guess={guessList[num] || '     '} />
       ))}
     </div>
   );
